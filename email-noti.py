ef0bd525b4f1407d3ed725fe9606d8fdd6406756
Jiraphat120 <76424510+Jiraphat120@users.noreply.github.com>
2023-12-18 18:00:13 +0700

    Update email-noti.py

--- email-noti.py
+++ email-noti.py
@@ -54,7 +54,7 @@ smtp_details = {
 
 send_email_with_attachments(
     "Scan Result", 
-    "jiraphat.g@got.co.th, {{RECEIVER}}",
+    "{{RECEIVER}}",
     ["jiraphat120@hotmail.com", "jiraphat.g@got.co.th"],
     # "itinfra@tlt.co.th",
     # ["pradist_k@tlt.co.th", "kijnipat_s@tlt.co.th", "kalunyu_s@tlt.co.th", "phornmesa_k@tlt.co.th", "jitrada_b@tlt.co.th", "jiraphat.g@got.co.th"], 
